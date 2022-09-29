2f3b1445a6beaedb4f5b6c407103d8479e2687ec
krapivnitskiy <makartut374@gmail.com>
2022-09-29 17:42:34 +0300

    added elements

--- ui/src/main/java/elements/fragments/HeaderFragment.java
+++ ui/src/main/java/elements/fragments/HeaderFragment.java
@@ -1,7 +1,27 @@
 package elements.fragments;
 
 import com.codeborne.selenide.ElementsContainer;
+import com.codeborne.selenide.SelenideElement;
+import elements.modals.CustomerLoginModal;
+import org.openqa.selenium.support.FindBy;
 
 public class HeaderFragment extends ElementsContainer {
 
+    @FindBy(css = "#login")
+    private SelenideElement loginButton;
+
+    @FindBy(css = "#register")
+    private SelenideElement registerButton;
+
+    @FindBy(css = "#tabIndex")
+    private SelenideElement homeButton;
+
+    @FindBy(css = "#tabCatalogue")
+    private SelenideElement catalogueButton;
+
+    @FindBy(css = ".modal-content")
+    private CustomerLoginModal customerLoginModal;
+
+    @FindBy(css = ".modal-content")
+    private CustomerLoginModal registerModal;
 }
