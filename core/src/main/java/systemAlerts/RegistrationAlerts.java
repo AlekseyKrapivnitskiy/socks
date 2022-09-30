8771ab66feed81609f7244b46eb84758419daa6f
krapivnitskiy <makartut374@gmail.com>
2022-09-30 19:24:42 +0300

    added class for texts

---
+++ core/src/main/java/systemAlerts/RegistrationAlerts.java
@@ -0,0 +1,7 @@
+package systemAlerts;
+
+public class RegistrationAlerts {
+
+    public static final String REGISTRATION_SUCCESSFUL = "Registration and login successful.";
+    public static final String REGISTRATION_PROBLEM = "There was a problem with your registration: Internal Server Error";
+}
