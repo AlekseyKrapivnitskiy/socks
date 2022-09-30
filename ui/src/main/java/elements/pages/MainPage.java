0ff24c41ee270a991dfe7eb01911814311425395
krapivnitskiy <makartut374@gmail.com>
2022-09-30 12:16:00 +0300

    added header fragments

--- ui/src/main/java/elements/pages/MainPage.java
+++ ui/src/main/java/elements/pages/MainPage.java
@@ -1,4 +1,14 @@
 package elements.pages;
 
+import elements.fragments.NavigationBarFragment;
+import elements.fragments.TopBarFragment;
+import org.openqa.selenium.support.FindBy;
+
 public class MainPage {
+
+    @FindBy(css = "#topbar")
+    private TopBarFragment topBar;
+
+    @FindBy(css = "#navbar")
+    private NavigationBarFragment navigationBar;
 }
