d5f4837a8aa44dbc4db2726d13a4c4b1a9113aa8
krapivnitskiy <makartut374@gmail.com>
2022-09-30 11:43:56 +0300

    implement base test class

--- ui/src/main/java/utils/BaseTest.java
+++ ui/src/main/java/utils/BaseTest.java
@@ -3,6 +3,8 @@ package utils;
 import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.browserSize;
 import static utils.ConfigProperties.getProperty;
 
 public class BaseTest {
@@ -10,6 +12,7 @@ public class BaseTest {
     @BeforeSuite
     public void beforeSuite() {
         baseUrl = getProperty("baseUrl");
-
+        browser = "chrome";
+        browserSize = "1920x1080";
     }
 }
