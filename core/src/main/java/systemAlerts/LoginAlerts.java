1e2a44c53103331baf177901ee293acb3341b92e
krapivnitskiy <makartut374@gmail.com>
2022-09-30 20:08:27 +0300

    added login alerts

---
+++ core/src/main/java/systemAlerts/LoginAlerts.java
@@ -0,0 +1,7 @@
+package systemAlerts;
+
+public class LoginAlerts {
+
+    public static final String LOGIN_SUCCESSFUL = "Login successful.";
+    public static final String INVALID_CREDENTIALS = "Invalid login credentials.";
+}
