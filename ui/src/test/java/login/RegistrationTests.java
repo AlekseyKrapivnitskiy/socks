d344bc0886c01f80e3a9d9a32075e0476f714c4d
krapivnitskiy <makartut374@gmail.com>
2022-10-05 10:40:51 +0300

    first test

---
+++ ui/src/test/java/login/RegistrationTests.java
@@ -0,0 +1,43 @@
+package login;
+
+import elements.pages.HomePage;
+import entities.Customer;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.BaseTest;
+import utils.passwordGenerator.PasswordPolicy;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+import static java.util.Locale.ENGLISH;
+import static testDataGenerators.CustomerGenerator.generatedAllFieldsCustomer;
+import static utils.ConfigProperties.getProperty;
+import static utils.passwordGenerator.PasswordGenerator.generatePassword;
+
+public class RegistrationTests extends BaseTest {
+
+    private HomePage homePage;
+    private Customer customer;
+
+    @BeforeMethod
+    void beforeMethod() {
+        homePage = page(HomePage.class);
+        String password = generatePassword(PasswordPolicy.builder().build());
+        customer = generatedAllFieldsCustomer(ENGLISH, password);
+    }
+
+    @Test
+    void registerPositive() {
+        open(getProperty("baseUrl"));
+
+        homePage.openRegisterModal();
+        homePage.fillRegisterModalClickOnRegisterButton(customer);
+        homePage.checkCustomerLoggedIn(customer);
+
+        homePage.logout();
+
+        homePage.openCustomerLoginModal();
+        homePage.fillCustomerLoginModalClickOnLogInButton(customer);
+        homePage.checkCustomerLoggedIn(customer);
+    }
+}
